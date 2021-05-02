944cfa0e7efa2a71e1f7edfa514fd5e9f750b1dc
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-05-02 11:14:57 +0300

    completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const myName = 'Vadim';
+
+  return (
+    <div className="App">
+      <a>Hello, {myName}</a>
+    </div>
+  );
+}
+
+export default App;
